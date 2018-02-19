1b8a51b56de5f8cfd161e934260dad24ff5b741f
lakshay kumar <kumar.lakshay1991@gmail.com>
2018-02-19 16:25:01 +0530

    added Home app and made necessary changes

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from Tutorial import views
 from django.conf import settings
 from django.conf.urls.static import static
+import home
 
 urlpatterns = [
 
     # below accounts.login is redirected with help of settings.LOGIN_
     #path('accounts/login/', views.reset_login_redirect, name='reset_login_redirect'),
-    path('', views.useraccount_homepage_redirect, name='useraccount_homepage_redirect'),
+    #path('', views.useraccount_homepage_redirect, name='useraccount_homepage_redirect'),
     path('useraccount/', include('ImageAccount.urls'), name='useraccount_homepage'),
+    path('home/', include('home.urls'), name='home_homepage'),
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from home.views import HomeView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name = 'home_index'),
+
+]
