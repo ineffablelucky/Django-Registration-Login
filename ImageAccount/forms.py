aa6abd44bff451179067760cda2299e720d68218
Lakshay Kumar <33530741+ineffablelucky@users.noreply.github.com>
2018-02-23 19:14:14 +0530

    Removed password section from Edit profile page

--- ImageAccount/forms.py
+++ ImageAccount/forms.py
@@ -28,14 +28,20 @@ class RegisterationForm(UserCreationForm):
 
         return user
 
-
-class EditProfileForm(UserChangeForm):
+    
+    
+class EditProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
         fields = (
-            
             'first_name',
             'last_name',
-            'password'
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self.fields['password'].help_text = self.fields['password'].help_text.format('../password/')
+        f = self.fields.get('user_permissions')
+        if f is not None:
+            f.queryset = f.queryset.select_related('content_type')
